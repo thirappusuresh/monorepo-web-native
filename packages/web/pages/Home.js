3e56a9c4d104847a8ab297a9dbd1129608cc54bb
Suresh Babu <suresh.t@medlife.com>
2018-12-03 19:06:08 +0530

    loaded App under menu

--- packages/web/pages/Home.js
+++ packages/web/pages/Home.js
@@ -20,8 +20,8 @@ class Home extends Component {
         return (
             <Fragment>
                 <Header />
-                <App />
                 <NavbarContainer />
+                <App />
                 <BodyWrapperContainer />
                 <Footer />
             </Fragment>
